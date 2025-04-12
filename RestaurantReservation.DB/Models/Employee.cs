855cded88df86e923e5a1d8b5decfb8234a9fcc2
Ruba <s12028675@stu.najah.edu>
2025-04-13 01:53:59 +0300

    Create Employee & Position Models

---
+++ RestaurantReservation.DB/Models/Employee.cs
@@ -0,0 +1,12 @@
+namespace RestaurantReservation.DB.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public int PositionId { get; set; }
+    public required Position Position { get; set; }
+    public int RestaurantId { get; set; }
+    public required Restaurant Restaurant { get; set; }
+}
---
+++ RestaurantReservation.DB/Models/Position.cs
@@ -0,0 +1,8 @@
+namespace RestaurantReservation.DB.Models;
+
+public class Position
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public List<Employee> Employees { get; set; } = [];
+}

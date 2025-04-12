9e6c506d1cc6027acbb83658af98e21e7e91e64c
Ruba <s12028675@stu.najah.edu>
2025-04-13 01:19:06 +0300

    Create Reservation Model

---
+++ RestaurantReservation.DB/Models/Reservation.cs
@@ -0,0 +1,14 @@
+namespace RestaurantReservation.DB.Models;
+
+public class Reservation
+{
+    public int Id { get; set; }
+    public DateTime ReservationDate { get; set; }
+    public int PartySize { get; set; }
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; }
+    public int TableId { get; set; }
+    public Table Table { get; set; }
+    public int RestaurantId { get; set; }
+    public Restaurant Restaurant { get; set; }
+}

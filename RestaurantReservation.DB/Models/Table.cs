be1ae5deabb2985973245590e1c7fcd32c395002
Ruba <s12028675@stu.najah.edu>
2025-04-13 03:28:33 +0300

    Update Order & Table Models: Add List of Reservation to Table Model

--- RestaurantReservation.DB/Models/Table.cs
+++ RestaurantReservation.DB/Models/Table.cs
@@ -5,6 +5,6 @@ public class Table
     public int Id { get; set; }
     public int Capacity { get; set; }
     public int RestaurantId { get; set; }
-    public Restaurant? Restaurant { get; set; }
-    public List<Reservation>? Reservations { get; set; }
+    public required Restaurant Restaurant { get; set; }
+    public List<Reservation> Reservations { get; set; } = [];
 }

47cff7e1a247f38d40270ed6a8ecec66428b4ee8
Ruba <s12028675@stu.najah.edu>
2025-04-16 01:57:33 +0300

    Create ListOrderedMenuItems method

--- RestaurantReservation.DB/Repositories/Interfaces/IMenuItemRepository.cs
+++ RestaurantReservation.DB/Repositories/Interfaces/IMenuItemRepository.cs
@@ -2,4 +2,7 @@ using RestaurantReservation.DB.Models.Entities;
 
 namespace RestaurantReservation.DB.Repositories.Interfaces;
 
-public interface IMenuItemRepository : IBaseRepository<MenuItem>;
+public interface IMenuItemRepository : IBaseRepository<MenuItem>
+{
+    Task<List<MenuItem>> ListOrderedMenuItems(int reservationId);
+}
--- RestaurantReservation.DB/Repositories/MenuItemRepository.cs
+++ RestaurantReservation.DB/Repositories/MenuItemRepository.cs
@@ -1,7 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.DB.Exceptions;
 using RestaurantReservation.DB.Models.Entities;
 using RestaurantReservation.DB.Repositories.Interfaces;
 
 namespace RestaurantReservation.DB.Repositories;
 
 public class MenuItemRepository(RestaurantReservationDbContext context)
-    : BaseRepository<MenuItem>(context), IMenuItemRepository;
+    : BaseRepository<MenuItem>(context), IMenuItemRepository
+{
+    private readonly RestaurantReservationDbContext _context = context;
+
+    public async Task<List<MenuItem>> ListOrderedMenuItems(int reservationId)
+    {
+        var isExist = await IsEntityExist(reservationId);
+        if (!isExist)
+        {
+            throw new NoRecordFoundException();
+        }
+        
+        return await _context.OrderItems
+            .Where(o => o.Order.ReservationId == reservationId)
+            .Select(o => o.MenuItem)
+            .ToListAsync();
+    }
+}

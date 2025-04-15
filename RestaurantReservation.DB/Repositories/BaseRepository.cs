95a464424a58001f3145e62e27bd3ac99a67e91f
Ruba <s12028675@stu.najah.edu>
2025-04-16 01:00:12 +0300

    Create Generic Repository for CRUD Operations

--- RestaurantReservation.DB/Repositories/BaseRepository.cs
+++ RestaurantReservation.DB/Repositories/BaseRepository.cs
@@ -1,6 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.DB.Exceptions;
+using RestaurantReservation.DB.Models.Interfaces;
+using RestaurantReservation.DB.Repositories.Interfaces;
+
 namespace RestaurantReservation.DB.Repositories;
 
-public class BaseRepository
+public class BaseRepository<T>(RestaurantReservationDbContext context) : IBaseRepository <T> where T : class, IEntity
 {
+    private readonly DbSet<T> _entitySet = context.Set<T>();
     
+    public async Task AddAsync(T entity)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+        
+        await _entitySet.AddAsync(entity);
+        await context.SaveChangesAsync();
+    }
+
+    public virtual async Task DeleteAsync(T entity)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+        var isExist = await IsEntityExist(entity.Id);
+        if (!isExist)
+        {
+            throw new NoRecordFoundException("No record found");
+        }
+        _entitySet.Remove(entity);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+        
+        var isExist = await IsEntityExist(entity.Id);
+        if (!isExist)
+        {
+            throw new NoRecordFoundException("No record found");
+        }
+        
+        _entitySet.Update(entity);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task<List<T>> GetAllAsync()
+    {
+        var data = await _entitySet.ToListAsync();
+        return data;
+    }
+
+    public async Task<bool> IsEntityExist(int id)
+    {
+        return await _entitySet.AnyAsync(e => e.Id == id);
+    }
 }
--- RestaurantReservation.DB/Repositories/Interfaces/IBaseRepository.cs
+++ RestaurantReservation.DB/Repositories/Interfaces/IBaseRepository.cs
@@ -1,6 +1,10 @@
 namespace RestaurantReservation.DB.Repositories.Interfaces;
 
-public interface IBaseRepository
+public interface IBaseRepository <T>
 {
-    
+    public Task AddAsync(T entity);
+    public Task DeleteAsync(T entity);
+    public Task UpdateAsync(T entity);
+    public Task<List<T>> GetAllAsync();
+    public Task<bool> IsEntityExist(int id);
 }

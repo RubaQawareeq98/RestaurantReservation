ab56236829f0604af2fec0b2d1ba51a41d1398c7
Ruba <s12028675@stu.najah.edu>
2025-04-16 18:28:02 +0300

    Refactor Program class: Apply Dependency Injection

--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using RestaurantReservation.DB;
 using RestaurantReservation.DB.Models.Entities;
 using RestaurantReservation.DB.Repositories.Interfaces;
@@ -8,11 +10,10 @@ namespace RestaurantReservation;
 
 static class Program
 {
-    private static RestaurantReservationDbContext _context = new RestaurantReservationDbContext();
+    private const string Message = "Please enter a valid integer";
 
-    private static async Task CallCustomerCrudOperations()
+    private static async Task CallCustomerCrudOperations(ICustomerRepository customerRepository)
     {
-        ICustomerRepository customerRepository = new CustomerRepository( _context );
         var customer = new Customer { FirstName = "Eman", LastName = "Ahmad", Email = "emanahmad@gmail.com", PhoneNumber = "123456789" };
         await customerRepository.AddAsync(customer);
         
@@ -26,10 +27,8 @@ static class Program
         customers.ForEach(Console.WriteLine);
     }
     
-    private static async Task CallEmployeeCrudOperations()
+    private static async Task CallEmployeeCrudOperations(IEmployeeRepository employeeRepository)
     {
-        IEmployeeRepository employeeRepository = new EmployeeRepository(_context);
-        
         var employee = new Employee { FirstName = "Eman", LastName = "Ahmad", PositionId = 2, RestaurantId = 3};
         await employeeRepository.AddAsync(employee);
 
@@ -43,10 +42,8 @@ static class Program
         employees.ForEach(Console.WriteLine);
     }
  
-    private static async Task CallRestaurantCrudOperations()
+    private static async Task CallRestaurantCrudOperations(IRestaurantRepository restaurantRepository)
     {
-        IRestaurantRepository restaurantRepository = new RestaurantRepository(_context);
-        
         var restaurant = new Restaurant {Name = "Restaurant7", Address = "Nablus", PhoneNumber = "123456789"};
         await restaurantRepository.AddAsync(restaurant);
 
@@ -60,11 +57,9 @@ static class Program
         restaurants.ForEach(Console.WriteLine);
     }
  
-    private static async Task CallReservationCrudOperations()
+    private static async Task CallReservationCrudOperations(IReservationRepository reservationRepository)
     {
-        IReservationRepository reservationRepository = new ReservationRepository(_context);
-        
-        var reservation = new Reservation { CustomerId = 2, RestaurantId = 3, TableId = 1, PartySize = 5, ReservationDate = DateTime.Now };
+        var reservation = new Reservation { CustomerId = 7, RestaurantId = 3, TableId = 1, PartySize = 5, ReservationDate = DateTime.Now };
         await reservationRepository.AddAsync(reservation);
 
         reservation.PartySize = 7;
@@ -77,10 +72,8 @@ static class Program
         restaurants.ForEach(Console.WriteLine);
     }
 
-    private static async Task CallTableCrudOperations()
+    private static async Task CallTableCrudOperations(ITableRepository tableRepository)
     {
-        ITableRepository tableRepository = new TableRepository(_context);
-
         var table = new Table { RestaurantId = 4, Capacity = 5};
         await tableRepository.AddAsync(table);
 
@@ -94,10 +87,8 @@ static class Program
         restaurants.ForEach(Console.WriteLine);
     }
 
-    private static async Task CallOrderCrudOperations()
+    private static async Task CallOrderCrudOperations(IOrderRepository orderRepository)
     {
-        IOrderRepository orderRepository = new OrderRepository(_context);
-
         var order = new Order
         {
             ReservationId = 4, OrderDate = DateTime.Now, EmployeeId = 2,
@@ -115,10 +106,8 @@ static class Program
         restaurants.ForEach(Console.WriteLine);
     }
 
-    private static async Task CallOrderItemCrudOperations()
+    private static async Task CallOrderItemCrudOperations(IOrderItemRepository orderItemRepository)
     {
-        IOrderItemRepository orderItemRepository = new OrderItemRepository(_context);
-
         var orderItem = new OrderItem { OrderId = 2, MenuItemId = 1, Quantity = 7 };
         await orderItemRepository.AddAsync(orderItem);
 
@@ -132,10 +121,8 @@ static class Program
         restaurants.ForEach(Console.WriteLine);
     }
 
-    private static async Task CallMenuItemCrudOperations()
+    private static async Task CallMenuItemCrudOperations(IMenuItemRepository menuItemRepository)
     {
-        IMenuItemRepository menuItemRepository = new MenuItemRepository(_context);
-
         var menuItem = new MenuItem { RestaurantId = 4, Description = "New Item", Name = "Item7", Price = 108 };
         await menuItemRepository.AddAsync(menuItem);
 
@@ -149,9 +136,8 @@ static class Program
         restaurants.ForEach(Console.WriteLine);
     }
 
-    private static async Task ListManagers()
+    private static async Task ListManagers(IEmployeeRepository employeeRepository)
     {
-        IEmployeeRepository employeeRepository = new EmployeeRepository(_context);
         var managers = await employeeRepository.GetManagersAsync();
 
         if (managers.Count == 0)
@@ -161,15 +147,14 @@ static class Program
         managers.ForEach(Console.WriteLine);
     }
 
-    private static async Task GetReservationsByCustomer()
+    private static async Task GetReservationsByCustomer(IReservationRepository reservationRepository)
     {
         Console.WriteLine("Getting Enter the Customer ID");
         var isValid = int.TryParse(Console.ReadLine(), out var customerId);
         if (!isValid)
         {
-            throw new ArgumentException("Please enter a valid integer");
+            throw new ArgumentException(Message);
         }
-        IReservationRepository reservationRepository = new ReservationRepository(_context);
         var reservations = await reservationRepository.GetReservationsByCustomer(customerId);
 
         if (reservations.Count == 0)
@@ -179,9 +164,15 @@ static class Program
         reservations.ForEach(Console.WriteLine);
     }
 
-    private static async Task ListOrdersAndMenuItems(int reservationId)
+    private static async Task ListOrdersAndMenuItems(IOrderRepository orderRepository)
     {
-        IOrderRepository orderRepository = new OrderRepository(_context);
+        Console.WriteLine("Getting Enter the Reservation ID");
+        var isValid = int.TryParse(Console.ReadLine(), out var reservationId);
+        if (!isValid)
+        {
+            throw new ArgumentException(Message);
+        }
+        
         var orders = await orderRepository.ListOrdersAndMenuItems(reservationId);
         foreach (var order in orders)
         {
@@ -193,9 +184,15 @@ static class Program
         }
     }
 
-    private static async Task ListOrderedMenuItems(int reservationId)
+    private static async Task ListOrderedMenuItems(IMenuItemRepository menuItemRepository)
     {
-        IMenuItemRepository menuItemRepository = new MenuItemRepository(_context);
+        Console.WriteLine("Getting Enter the Reservation ID");
+        var isValid = int.TryParse(Console.ReadLine(), out var reservationId);
+        if (!isValid)
+        {
+            throw new ArgumentException(Message);
+        }
+
         var menuItems = await menuItemRepository.ListOrderedMenuItems(reservationId);
         if (menuItems.Count == 0)
         {
@@ -204,42 +201,76 @@ static class Program
         menuItems.ForEach(Console.WriteLine);
     }
 
-    private static async Task CalculateAverageOrderAmount(int employeeId)
+    private static async Task CalculateAverageOrderAmount(IEmployeeRepository employeeRepository)
     {
-        IEmployeeRepository employeeRepository = new EmployeeRepository(_context);
+        Console.WriteLine("Getting Enter the Employee ID");
+        var isValid = int.TryParse(Console.ReadLine(), out var employeeId);
+        if (!isValid)
+        {
+            throw new ArgumentException(Message);
+        }
+
         var averageOrderAmount = await employeeRepository.CalculateAverageOrderAmount(employeeId);
         Console.WriteLine($"Average order amount: {averageOrderAmount}");
     }
 
-    private static async Task QueryReservationDetailsView()
+    private static async Task QueryReservationDetailsView(RestaurantReservationDbContext context)
     {
-        var reservationDetails = await _context.ReservationDetails.ToListAsync();
+        var reservationDetails = await context.ReservationDetails.ToListAsync();
         reservationDetails.ForEach(Console.WriteLine);
     }
     
-    private static async Task QueryEmployeeWithRestaurantDetailsView()
+    private static async Task QueryEmployeeWithRestaurantDetailsView(RestaurantReservationDbContext context)
     {
-        var reservationDetails = await _context.EmployeeWithRestaurantDetails.ToListAsync();
+        var reservationDetails = await context.EmployeeWithRestaurantDetails.ToListAsync();
         reservationDetails.ForEach(Console.WriteLine);
     }
     
     static async Task Main()
     {
-        // await CallCustomerCrudOperations();
-        // await CallEmployeeCrudOperations();
-        // await CallRestaurantCrudOperations();
-        // await CallReservationCrudOperations();
-        // await CallTableCrudOperations();
-        // await CallOrderCrudOperations();
-        // await CallOrderItemCrudOperations();
-        // await CallMenuItemCrudOperations();
-        // await ListManagers();
-        // await GetReservationsByCustomer();
-
-        //await ListOrdersAndMenuItems(1);
-        //await ListOrderedMenuItems(4);
-        //await CalculateAverageOrderAmount(4);
-        //await QueryReservationDetailsView();
-        await QueryEmployeeWithRestaurantDetailsView();
+        using IHost host = Host.CreateDefaultBuilder()
+            .ConfigureServices((_, services) =>
+            {
+                services.AddDbContext<RestaurantReservationDbContext>();
+                services.AddScoped<ICustomerRepository, CustomerRepository>();
+                services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+                services.AddScoped<IReservationRepository, ReservationRepository>();
+                services.AddScoped<IOrderRepository, OrderRepository>();
+                services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+                services.AddScoped<IMenuItemRepository, MenuItemRepository>();
+                services.AddScoped<IRestaurantRepository, RestaurantRepository>();
+                services.AddScoped<ITableRepository, TableRepository>();
+            })
+            .Build();
+        
+        using var scope = host.Services.CreateScope();
+        var services = scope.ServiceProvider;
+        
+        var context = services.GetRequiredService<RestaurantReservationDbContext>();
+        
+        var customerRepo = services.GetRequiredService<ICustomerRepository>();
+        var employeeRepo = services.GetRequiredService<IEmployeeRepository>();
+        var orderRepo = services.GetRequiredService<IOrderRepository>();
+        var orderItemRepo = services.GetRequiredService<IOrderItemRepository>();
+        var restaurantRepo = services.GetRequiredService<IRestaurantRepository>();
+        var reservationRepo = services.GetRequiredService<IReservationRepository>();
+        var menuItemRepo = services.GetRequiredService<IMenuItemRepository>();
+        var tableRepository = services.GetRequiredService<ITableRepository>();
+        
+        await CallCustomerCrudOperations(customerRepo);
+        await CallEmployeeCrudOperations(employeeRepo);
+        await CallRestaurantCrudOperations(restaurantRepo);
+        await CallReservationCrudOperations(reservationRepo);
+        await CallTableCrudOperations(tableRepository);
+        await CallOrderCrudOperations(orderRepo);
+        await CallOrderItemCrudOperations(orderItemRepo);
+        await CallMenuItemCrudOperations(menuItemRepo);
+        await ListManagers(employeeRepo);
+        await GetReservationsByCustomer(reservationRepo);
+        await ListOrdersAndMenuItems(orderRepo);
+        await ListOrderedMenuItems(menuItemRepo);
+        await CalculateAverageOrderAmount(employeeRepo);
+        await QueryReservationDetailsView(context);
+        await QueryEmployeeWithRestaurantDetailsView(context);
     }
 }

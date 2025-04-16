058613943d4ad9901eb415f248cb8dbd3e3e70a2
Ruba <s12028675@stu.najah.edu>
2025-04-16 12:39:35 +0300

    Call CRUD methods for Customer Repository

--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace RestaurantReservation;
+﻿using RestaurantReservation.DB;
+using RestaurantReservation.DB.Models.Entities;
+using RestaurantReservation.DB.Repositories.Interfaces;
+using RestaurantReservation.DB.Repositories.Repos;
+
+namespace RestaurantReservation;
 
 class Program
 {
-    static void Main(string[] args)
+    private static RestaurantReservationDbContext _context = new RestaurantReservationDbContext();
+
+    private static async Task TestCustomerCrudOperations()
     {
-        Console.WriteLine("Hello, World!");
+        ICustomerRepository customerRepository = new CustomerRepository( _context );
+        var customer = new Customer { FirstName = "Eman", LastName = "Ahmad", Email = "emanahmad@gmail.com", PhoneNumber = "123456789" };
+        await customerRepository.AddAsync(customer);
+        
+        customer.PhoneNumber = "059789456";
+        await customerRepository.UpdateAsync(customer);
+
+        var customers = await customerRepository.GetAllAsync();
+        var deletedCustomer = customers.FirstOrDefault();
+        await customerRepository.DeleteAsync(deletedCustomer);
+        
+        customers.ForEach(Console.WriteLine);
+    }
+
+    static async Task Main(string[] args)
+    {
+        await TestCustomerCrudOperations();
     }
 }

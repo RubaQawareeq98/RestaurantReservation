c8c38a4e7fee770c9c83a2a3ee10b8bb96b06fef
Ruba <s12028675@stu.najah.edu>
2025-04-16 04:40:54 +0300

    Add Migration for Creating CalculateTotalRevenueFunction

---
+++ RestaurantReservation.DB/Migrations/20250416010217_CalculateTotalRevenueFunction.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class CalculateTotalRevenueFunction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+	        migrationBuilder.Sql(@"CREATE FUNCTION fn_CalculateTotalRevenue (@restaurantId INT)
+									RETURNS decimal(6,2) AS
+									BEGIN
+										DECLARE @totalRevenue decimal(6,2);
+
+											WITH RestaurantItems AS
+												(
+													SELECT oi.quantity as quantity,
+													i.price as price
+													from Reservations r
+													join Orders o on r.Id = o.ReservationId
+													join OrderItems oi on o.Id = oi.OrderId
+													join MenuItems i on oi.MenuItemId = i.Id
+													where r.RestaurantId = 1
+												)
+											SELECT @totalRevenue = SUM(quantity * price)
+											FROM RestaurantItems
+
+											RETURN @totalRevenue
+									END");	
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

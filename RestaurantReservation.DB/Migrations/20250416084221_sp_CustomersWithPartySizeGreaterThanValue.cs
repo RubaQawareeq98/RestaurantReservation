f7a39a3be9098e5be6564c8c95494e5d3bddd653
Ruba <s12028675@stu.najah.edu>
2025-04-16 11:46:12 +0300

    Add Migration to Create Procedur CustomersWithPartySizeGreaterThanValue

---
+++ RestaurantReservation.DB/Migrations/20250416084221_sp_CustomersWithPartySizeGreaterThanValue.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class CustomersWithReservationsGreateThanPartySizeProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+	        migrationBuilder.Sql(@"CREATE PROCEDURE sp_CustomersWithPartySizeGreaterThanValue (@PartySize INT)
+									AS
+									BEGIN
+										select c.*
+										from Reservations r
+										Join Customers c
+										on r.CustomerId = c.Id
+										where r.PartySize > @partySize
+									END");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+	        migrationBuilder.Sql("DROP PROCEDURE sp_CustomersWithPartySizeGreaterThanValue");
+        }
+    }
+}

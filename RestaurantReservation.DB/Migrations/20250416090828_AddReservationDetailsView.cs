ac3c7333164fbf2a674a561d3a89dcfed1a99ce8
Ruba <s12028675@stu.najah.edu>
2025-04-16 12:11:56 +0300

    Update Reservation Details View Migration

---
+++ RestaurantReservation.DB/Migrations/20250416090828_AddReservationDetailsView.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReservationDetailsView : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE VIEW [dbo].[ReservationDetails]
+                                  As SELECT 
+                                        r.Id AS ReservationId,
+                                        r.ReservationDate,
+                                        r.PartySize,
+                                        r.TableId,
+                                        c.Id AS CustomerId,
+                                        c.FirstName AS CustomerFirstName,
+                                        c.LastName AS CustomerLastName,
+                                        c.PhoneNumber AS CustomerPhone,
+                                        c.Email AS CustomerEmail,
+                                        res.Id AS RestaurantId,
+                                        res.Name AS RestaurantName,
+                                        res.Address AS RestaurantAddress,
+			                            CONCAT(o.DayOfWeek, o.OpenHour, ' - ', o.CloseHour) as OpenHour
+			                            
+                                    FROM Reservations r
+                                    LEFT JOIN Customers c ON r.CustomerId = c.Id
+                                    LEFT JOIN Restaurants res ON r.RestaurantId = res.Id
+		                            LEFT JOIN OpeningHours o ON r.Id = o.RestaurantId
+                            ");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW [dbo].[ReservationDetails]");
+        }
+    }
+}
